d272b31b928db93c7ca1fffcad499f4e61e8e3f9
mucarb <murilo.rb60@gmail.com>
2022-05-30 14:42:01 -0300

    Slider em tela cheia

---
+++ script.js
@@ -0,0 +1,34 @@
+let totalSlides = document.querySelectorAll('.slider-item').length
+let currentSlide = 0;
+
+document.querySelector('.slider-width').style.width = `calc(100vw * ${totalSlides})`;
+
+document.querySelector('.slider-coltrols').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+function goToPrevious() {
+    currentSlide--;
+
+    if (currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+}
+
+function goToNext() {
+    currentSlide++;
+
+    if(currentSlide > totalSlides-1){
+        currentSlide = 0;
+    }
+
+    updateMargin();
+}
+
+function updateMargin() {
+    let sliderItemWidth = document.querySelector('.slider-item').clientWidth;
+    let newMargin = currentSlide * sliderItemWidth;
+
+    document.querySelector('.slider-width').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goToNext, 5000);
